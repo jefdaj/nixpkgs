03d2d7dcd73cb6d167c8d61543060a6bc08c7158
Jeffrey David Johnson <jefdaj@gmail.com>
2016-11-20 01:19:45 -0800

    fasttree: init at 2.1.9

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22851,6 +22851,8 @@ in
 
   exonerate = callPackage ../applications/science/biology/exonerate { };
 
+  fasttree = callPackage ../applications/science/biology/fasttree { };
+
   hisat2 = callPackage ../applications/science/biology/hisat2 { };
 
   htslib = callPackage ../development/libraries/science/biology/htslib { };
