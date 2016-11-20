893e9fa389492a2aef88e2dd4fec83ed7827a1a4
Jeffrey David Johnson <jefdaj@gmail.com>
2016-11-20 14:18:16 -0800

    viennarna: init at 2.3.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23658,6 +23658,8 @@ in
 
   varscan = callPackage ../applications/science/biology/varscan { };
 
+  viennarna = callPackage ../applications/science/biology/viennarna { };
+
   hmmer = callPackage ../applications/science/biology/hmmer { };
 
   bwa = callPackage ../applications/science/biology/bwa { };
