fd3b94e8a91161b1738d0e06ead840362461e121
Jeffrey David Johnson <jefdaj@gmail.com>
2016-11-20 06:38:00 -0800

    igvtools: init at 2.3.88

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22861,6 +22861,9 @@ in
 
   igv = callPackage ../applications/science/biology/igv { };
 
+  # TODO include this with igv above?
+  igvtools = callPackage ../applications/science/biology/igvtools { };
+
   inormalize = callPackage ../applications/science/biology/inormalize { };
 
   itsx = callPackage ../applications/science/biology/itsx { };
