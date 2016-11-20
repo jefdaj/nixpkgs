daa06c189a794130c2859eb90c69acb68b0ef0e1
Jeffrey David Johnson <jefdaj@gmail.com>
2016-11-20 14:58:10 -0800

    fix raxml hash

--- pkgs/applications/science/biology/raxml/default.nix
+++ pkgs/applications/science/biology/raxml/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "https://github.com/stamatak/standard-RAxML.git";
     rev = "4881d835222f3dcc3faec5f14d5681cb97de97c5";
-    sha256 = "0";
+    sha256 = "1bv12bzx7nqkhxp4wdfgl6wg4lx95j7vsg9mcj87b0hkr5zl9p2l";
   };
   buildInputs = [ clang mpich2 ];
   buildPhase = ''
