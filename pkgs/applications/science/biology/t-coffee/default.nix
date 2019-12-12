ecd23fab55a7582ac9bb08ff8efe612b1250ab6e
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-12-11 18:51:42 -0800

    t-coffee: 11.00.8cbe486 -> 13.41.0.28bdc39

--- pkgs/applications/science/biology/t-coffee/default.nix
+++ pkgs/applications/science/biology/t-coffee/default.nix
@@ -2,10 +2,10 @@
 
 stdenv.mkDerivation rec {
   name = "t-coffee-${version}";
-  version = "11.00.8cbe486";
+  version = "13.41.0.28bdc39";
   src = fetchurl {
-    url = "http://www.tcoffee.org/Packages/Stable/Latest/T-COFFEE_distribution_Version_${version}.tar.gz";
-    sha256 = "0b77g1vjwgzmnjgam2p3qn5ynj3an3mlp5yd95f71xsgawrq80dq";
+    url = "http://www.tcoffee.org/Packages/Archives/T-COFFEE_distribution_Version_${version}.tar.gz";
+    sha256 = "0hbhb3145hbviwy6gm077psw92yhjyx46m5hbngb0gvjhr7ya6q8";
   };
   builder = ./builder.sh;
 }
