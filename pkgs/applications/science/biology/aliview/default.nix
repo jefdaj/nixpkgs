f4c96ffd2541117231e27ef8d32f6eee74347094
Jeffrey David Johnson <jefdaj@berkeley.edu>
2019-12-11 18:46:35 -0800

    fix aliview url

--- pkgs/applications/science/biology/aliview/default.nix
+++ pkgs/applications/science/biology/aliview/default.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   name = "aliview-${version}";
   version = "1.18";
   src = fetchurl {
-    url = "http://www.ormbunkar.se/aliview/downloads/linux/linux-version-1.18/aliview.tgz";
+    url = "http://www.ormbunkar.se/aliview/downloads/linux/linux-versions-all/linux-version-1.18/aliview.tgz";
     sha256 = "1m7ign2wb62fn4hmi4ywnkzdpjifl6jz83l27i40nby7cyyym9cj";
   };
   inherit jre;
